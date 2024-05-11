cfe07ea2a45cdf44ba7197cb6d1fa40c7d156c8b
Raido Oras <oras.raido@gmail.com>
2024-05-11 03:10:42 +0300

    feat: better textnode validations

--- src/textnode.py
+++ src/textnode.py
@@ -1,8 +1,21 @@
 class TextNode:
-    def __init__(self, text, text_type, url):
+    valid_types = ["bold", "italic", "strikethrough", "underline"]
+
+    def __init__(self, text, text_type, url=None):
+        if not isinstance(text, str):
+            raise TypeError("Text must be of type string")
+        if text_type is None or text_type not in self.valid_types:
+            raise TypeError("text_type must be one of " + ", ".join(self.valid_types))
+        if url is not None and not isinstance(url, str):
+            raise TypeError("url must be of type string")
+
+        if url == "":
+            self.url = None
+        else:
+            self.url = url
+
         self.text = text
         self.text_type = text_type
-        self.url = url
 
     def __eq__(self, other):
         return (self.text == other.text

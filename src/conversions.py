f2f4dde9a74cc63b7d5dc558d65d9326e22c2bc6
Raido Oras <oras.raido@gmail.com>
2024-06-03 03:35:38 +0300

    text_to_textnodes
    holy crap I think it worked

--- src/conversions.py
+++ src/conversions.py
@@ -1,3 +1,5 @@
+import re
+
 from src.htmlnode import HTMLNode
 from src.leafnode import LeafNode
 from src.textnode import TextNode
@@ -25,3 +27,145 @@ def text_node_to_html_node(node: TextNode) -> HTMLNode:
         return LeafNode("img", None, {'src': node.url, 'alt': node.text})
 
     raise ValueError(f"Text type {node.text_type} is not a valid text_type")
+
+
+text_zone = 0
+delim_zone = 1
+
+
+def map_into_zones(text, delimiter):
+    text_classification = []
+    delim_zone_buffer = 0
+    in_delim_zone = False
+
+    for idx, char in enumerate(text):
+        chunk = text[idx:idx + len(delimiter)]
+
+        if delim_zone_buffer > 0:
+            delim_zone_buffer -= 1
+            if delim_zone_buffer == 0:
+                in_delim_zone = False
+
+        if chunk == delimiter and in_delim_zone:
+            delim_zone_buffer = len(delimiter)
+        elif chunk == delimiter and not in_delim_zone:
+            in_delim_zone = True
+
+        if in_delim_zone or delim_zone_buffer > 0:
+            text_classification.append((char, delim_zone))
+        else:
+            text_classification.append((char, text_zone))
+
+    return text_classification
+
+
+def split_nodes_delimiter(old_nodes: [TextNode], delimiter: str, text_type: str) -> [TextNode]:
+    new_nodes = []
+    buffer = ""
+
+    for node in old_nodes:
+        if node.text_type != TextNode.text_type_text:
+            new_nodes.append(node)
+            continue
+
+        zones = map_into_zones(node.text, delimiter)
+
+        for idx, zone_col in enumerate(zones):
+            col_char = zone_col[0]
+            col_type = zone_col[1]
+            next_index = idx + 1
+            buffer += col_char
+
+            next_type = None
+            try:
+                next_type = zones[next_index][1]
+            except IndexError:
+                next_type = col_type + 1  # zone map ended. Make next check pass.
+
+            if next_type != col_type:
+                if col_type == delim_zone:
+                    new_nodes.append(TextNode(buffer.strip(delimiter), text_type))
+                else:
+                    new_nodes.append(TextNode(buffer, TextNode.text_type_text))
+                buffer = ""
+
+    return new_nodes
+
+
+def extract_markdown_images(text: str):
+    return re.findall(r"!\[(.*?)]\((.*?)\)", text)
+
+
+def extract_markdown_links(text: str):
+    return re.findall(r"(?<!!)\[(.*?)]\((.*?)\)", text)
+
+
+def split_nodes_image(old_nodes):
+    cursor = 0
+    counter = 0
+    new_nodes = []
+
+    for node in old_nodes:
+        text = node.text
+        imgs = extract_markdown_images(node.text)
+
+        if len(imgs) == 0:
+            new_nodes.append(node)
+
+        for img in imgs:
+            counter += 1
+            splitting_string = f"![{img[0]}]({img[1]})"
+            text_starting_at_cursor = text[cursor:]
+            split_text = text_starting_at_cursor.split(splitting_string, 1)
+
+            if len(split_text) > 1:
+                new_nodes.append(TextNode(split_text[0], TextNode.text_type_text))
+                cursor = len(split_text[0]) + len(splitting_string)
+                new_nodes.append(TextNode(img[0], TextNode.text_type_image, img[1]))
+
+                if counter == len(imgs) and len(split_text[1]) > 0:
+                    new_nodes.append(TextNode(split_text[1], TextNode.text_type_text))
+
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    cursor = 0
+    counter = 0
+    new_nodes = []
+
+    for node in old_nodes:
+        text = node.text
+        links = extract_markdown_links(node.text)
+
+        if len(links) == 0:
+            new_nodes.append(node)
+
+        for link in links:
+            counter += 1
+            splitting_string = f"[{link[0]}]({link[1]})"
+            text_starting_at_cursor = text[cursor:]
+            split_text = text_starting_at_cursor.split(splitting_string, 1)
+
+            if len(split_text) > 1:
+                new_nodes.append(TextNode(split_text[0], TextNode.text_type_text))
+                cursor = len(split_text[0]) + len(splitting_string)
+                new_nodes.append(TextNode(link[0], TextNode.text_type_link, link[1]))
+
+                if counter == len(links) and len(split_text[1]) > 0:
+                    new_nodes.append(TextNode(split_text[1], TextNode.text_type_text))
+
+    return new_nodes
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextNode.text_type_text)]
+
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_image(nodes)
+
+    nodes = split_nodes_delimiter(nodes, "**", TextNode.text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", TextNode.text_type_italic)
+    nodes = split_nodes_delimiter(nodes, "`", TextNode.text_type_code)
+
+    return nodes
--- src/tests/test_conversions.py
+++ src/tests/test_conversions.py
@@ -1,6 +1,7 @@
 import unittest
 
-from src.conversions import text_node_to_html_node
+from src.conversions import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, \
+    extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes, map_into_zones
 from src.textnode import TextNode
 
 
@@ -35,3 +36,92 @@ class TestTextNode(unittest.TestCase):
         node = text_node_to_html_node(TextNode("Hello", TextNode.text_type_image, "https://test.test"))
         self.assertEqual(node.tag, "img")
         self.assertEqual(node.props, {'src': "https://test.test", 'alt': "Hello"})
+
+    def test_split_nodes_delimiter(self):
+        node = TextNode("This is text with a `code block` word", TextNode.text_type_text)
+        new_nodes = split_nodes_delimiter([node], "`", TextNode.text_type_code)
+
+        expected = [
+            TextNode("This is text with a ", TextNode.text_type_text),
+            TextNode("code block", TextNode.text_type_code),
+            TextNode(" word", TextNode.text_type_text),
+        ]
+
+        self.assertEqual(new_nodes, expected)
+
+    def test_extract_images(self):
+        text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
+        self.assertEqual(extract_markdown_images(text), [
+            ("image", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+            ("another", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png")])
+
+    def test_extract_links(self):
+        text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+        self.assertEqual(extract_markdown_links(text),
+                         [("link", "https://www.example.com"), ("another", "https://www.example.com/another")])
+
+    def test_split_nodes_image(self):
+        node = TextNode(
+            "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png) and some more",
+            TextNode.text_type_text,
+        )
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode("This is text with an ", TextNode.text_type_text),
+            TextNode("image", TextNode.text_type_image,
+                     "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+            TextNode(" and another ", TextNode.text_type_text),
+            TextNode(
+                "second image", TextNode.text_type_image,
+                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png"
+            ),
+            TextNode(" and some more", TextNode.text_type_text),
+        ]
+        self.assertEqual(expected, new_nodes)
+
+    def test_split_nodes_link(self):
+        node = TextNode(
+            "This is text with an [linkyboi](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another [second linkyboi](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png) and some more",
+            TextNode.text_type_text,
+        )
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode("This is text with an ", TextNode.text_type_text),
+            TextNode("linkyboi", TextNode.text_type_link,
+                     "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+            TextNode(" and another ", TextNode.text_type_text),
+            TextNode(
+                "second linkyboi", TextNode.text_type_link,
+                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png"
+            ),
+            TextNode(" and some more", TextNode.text_type_text),
+        ]
+        self.assertEqual(expected, new_nodes)
+
+    def test_text_to_nodes(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)"
+        new_nodes = text_to_textnodes(text)
+        expected = [
+            TextNode("This is ", TextNode.text_type_text),
+            TextNode("text", TextNode.text_type_bold),
+            TextNode(" with an ", TextNode.text_type_text),
+            TextNode("italic", TextNode.text_type_italic),
+            TextNode(" word and a ", TextNode.text_type_text),
+            TextNode("code block", TextNode.text_type_code),
+            TextNode(" and an ", TextNode.text_type_text),
+            TextNode("image", TextNode.text_type_image, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+            TextNode(" and a ", TextNode.text_type_text),
+            TextNode("link", TextNode.text_type_link, "https://boot.dev"),
+        ]
+        self.assertEqual(expected, new_nodes)
+
+    def test_zone_mapper(self):
+        text = "This *boob* is text with a *hallo* bit bit ibitus"
+        res = map_into_zones(text, "*")
+        expected = [('T', 0), ('h', 0), ('i', 0), ('s', 0), (' ', 0), ('*', 1), ('b', 1), ('o', 1), ('o', 1), ('b', 1), ('*', 1), (' ', 0), ('i', 0), ('s', 0), (' ', 0), ('t', 0), ('e', 0), ('x', 0), ('t', 0), (' ', 0), ('w', 0), ('i', 0), ('t', 0), ('h', 0), (' ', 0), ('a', 0), (' ', 0), ('*', 1), ('h', 1), ('a', 1), ('l', 1), ('l', 1), ('o', 1), ('*', 1), (' ', 0), ('b', 0), ('i', 0), ('t', 0), (' ', 0), ('b', 0), ('i', 0), ('t', 0), (' ', 0), ('i', 0), ('b', 0), ('i', 0), ('t', 0), ('u', 0), ('s', 0)]
+        self.assertEqual(res, expected)
+
+        text = "This **boob** is text with a *hallo* bit bit ibitus"
+        res = map_into_zones(text, "**")
+        expected = [('T', 0), ('h', 0), ('i', 0), ('s', 0), (' ', 0), ('*', 1), ('*', 1), ('b', 1), ('o', 1), ('o', 1), ('b', 1), ('*', 1), ('*', 1), (' ', 0), ('i', 0), ('s', 0), (' ', 0), ('t', 0), ('e', 0), ('x', 0), ('t', 0), (' ', 0), ('w', 0), ('i', 0), ('t', 0), ('h', 0), (' ', 0), ('a', 0), (' ', 0), ('*', 0), ('h', 0), ('a', 0), ('l', 0), ('l', 0), ('o', 0), ('*', 0), (' ', 0), ('b', 0), ('i', 0), ('t', 0), (' ', 0), ('b', 0), ('i', 0), ('t', 0), (' ', 0), ('i', 0), ('b', 0), ('i', 0), ('t', 0), ('u', 0), ('s', 0)]
+        self.assertEqual(res, expected)

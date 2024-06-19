7b1a146e975ad2c267ad49b2879661c6ba018201
Raido Oras <oras.raido@gmail.com>
2024-06-19 23:40:30 +0300

    refactor conversions files

--- src/tests/test_conversions.py
+++ src/tests/test_conversions.py
@@ -1,7 +1,7 @@
 import unittest
 
-from src.conversions import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, \
-    extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes, map_into_zones, markdown_to_blocks
+from src.inline_conversions import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, \
+    extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes, map_into_zones
 from src.textnode import TextNode
 
 
@@ -136,14 +136,3 @@ class TestTextNode(unittest.TestCase):
                     ('s', 0)]
         self.assertEqual(res, expected)
 
-    def test_markdown_to_block(self):
-        md = """This is **bolded** paragraph
-
-This is another paragraph with *italic* text and `code` here
-This is the same paragraph on a new line
-
-* This is a list
-* with items"""
-
-        blocks = markdown_to_blocks(md)
-        self.assertEqual(len(blocks), 3)
